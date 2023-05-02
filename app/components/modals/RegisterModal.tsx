c72f36969cbea090f3c9ee957f596fdb54c81d05
ahmetenesdur <ahmetenesdur@gmail.com>
2023-05-02 22:45:29 +0300

    Auth UI Finished

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,7 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
 import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 interface RegisterModalProps {}
 
@@ -80,6 +81,35 @@ const RegisterModal: React.FC<RegisterModalProps> = ({}) => {
     </div>
   );
 
+  const footerContent = (
+    <div className="flex flex-col gap-4">
+      <hr />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="justify-center flex flex-row items-center gap-2">
+          <div>Already have an account? </div>
+          <div
+            onClick={registerModal.onClose}
+            className="text-neutral-800 cursor-pointer hover:underline"
+          >
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <Modal
       disable={isLoading}
@@ -89,6 +119,7 @@ const RegisterModal: React.FC<RegisterModalProps> = ({}) => {
       actionLabel="Continue"
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
